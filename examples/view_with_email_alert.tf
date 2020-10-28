7d283fcb2d61371c779bc6b51d0cf95142becf9a
Dhruv Madhok <dhruv.madhawk@gmail.com>
2020-10-28 15:47:22 -0700

    Adds tests (#6)
    Adds tests
    
    Improve coveralls coverage by testing updates and error-handling.
    
    Ref: LOG-7721
    Semver: Patch

--- examples/view_with_email_alert.tf
+++ examples/view_with_email_alert.tf
@@ -3,13 +3,13 @@ provider "logdna" {
 }
 
 resource "logdna_view" "my_view" {
-  apps     = ["app1", "app2"]
+  apps       = ["app1", "app2"]
   categories = ["Demo1", "Demo2"]
-  hosts    = ["host1", "host2"]
-  levels   = ["fatal", "critical"]
-  name     = "Email Alert"
-  query    = "test"
-  tags     = ["host1", "host2"]
+  hosts      = ["host1", "host2"]
+  levels     = ["fatal", "critical"]
+  name       = "Email Alert"
+  query      = "test"
+  tags       = ["host1", "host2"]
   email_channel {
     emails          = ["test@logdna.com"]
     operator        = "absence"
--- logdna/resource_view_test.go
+++ logdna/resource_view_test.go
@@ -181,6 +181,35 @@ func TestViewBasic(t *testing.T) {
 	})
 }
 
+func TestViewBasicUpdate(t *testing.T) {
+	name := "test"
+	query := "test"
+	name2 := "test2"
+	query2 := "test2"
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testViewConfigBasic(name, query),
+				Check: resource.ComposeTestCheckFunc(
+					testViewExists("logdna_view.new"),
+					resource.TestCheckResourceAttr("logdna_view.new", "name", name),
+					resource.TestCheckResourceAttr("logdna_view.new", "query", query),
+				),
+			},
+			{
+				Config: testViewConfigBasic(name2, query2),
+				Check: resource.ComposeTestCheckFunc(
+					testViewExists("logdna_view.new"),
+					resource.TestCheckResourceAttr("logdna_view.new", "name", name2),
+					resource.TestCheckResourceAttr("logdna_view.new", "query", query2),
+				),
+			},
+		},
+	})
+}
+
 func TestViewBulkEmails(t *testing.T) {
 	name := "test"
 	query := "test"
@@ -239,6 +268,128 @@ func TestViewBulkEmails(t *testing.T) {
 					resource.TestCheckResourceAttr("logdna_view.new", "tags.#", "2"),
 					resource.TestCheckResourceAttr("logdna_view.new", "tags.0", tags1),
 					resource.TestCheckResourceAttr("logdna_view.new", "tags.1", tags2),
+					resource.TestCheckResourceAttr("logdna_view.new", "webhook_channels.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestViewBulkEmailsUpdate(t *testing.T) {
+	name := "test"
+	query := "test"
+	app1 := "app1"
+	app2 := "app2"
+	levels1 := "fatal"
+	levels2 := "critical"
+	host1 := "host1"
+	host2 := "host2"
+	category := "DEMO"
+	category2 := "DEMO2"
+	tags1 := "host1"
+	tags2 := "host2"
+
+	name2 := "test2"
+	query2 := "query2"
+	app3 := "app3"
+	app4 := "app4"
+	levels3 := "error"
+	levels4 := "warning"
+	host3 := "host3"
+	host4 := "host4"
+	category3 := "DEMO3"
+	category4 := "DEMO4"
+	tags3 := "tags3"
+	tags4 := "tags4"
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testViewConfigBulkEmails(name, query, app1, app2, levels1, levels2, host1, host2, category, category2, tags1, tags2),
+				Check: resource.ComposeTestCheckFunc(
+					testViewExists("logdna_view.new"),
+					resource.TestCheckResourceAttr("logdna_view.new", "name", name),
+					resource.TestCheckResourceAttr("logdna_view.new", "query", query),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.0", app1),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.1", app2),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.0", category),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.1", category2),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.emails.#", "1"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.emails.0", "test@logdna.com"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.immediate", "false"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.operator", "absence"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.terminal", "true"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.timezone", "Pacific/Samoa"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.triggerinterval", "15m"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.triggerlimit", "15"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.%", "7"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.emails.#", "1"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.emails.0", "test@logdna.com"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.immediate", "false"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.operator", "absence"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.terminal", "true"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.timezone", "Pacific/Samoa"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.triggerinterval", "15m"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.triggerlimit", "15"),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.0", host1),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.1", host2),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.0", host1),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.0", levels1),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.1", levels2),
+					resource.TestCheckResourceAttr("logdna_view.new", "pagerduty_channel.#", "0"),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.0", host1),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.1", host2),
+					resource.TestCheckResourceAttr("logdna_view.new", "webhook_channels.#", "0"),
+				),
+			},
+			{
+				Config: testViewConfigBulkEmails(name2, query2, app3, app4, levels3, levels4, host3, host4, category3, category4, tags3, tags4),
+				Check: resource.ComposeTestCheckFunc(
+					testViewExists("logdna_view.new"),
+					resource.TestCheckResourceAttr("logdna_view.new", "name", name2),
+					resource.TestCheckResourceAttr("logdna_view.new", "query", query2),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.0", app3),
+					resource.TestCheckResourceAttr("logdna_view.new", "apps.1", app4),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.0", category3),
+					resource.TestCheckResourceAttr("logdna_view.new", "categories.1", category4),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.emails.#", "1"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.emails.0", "test@logdna.com"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.immediate", "false"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.operator", "absence"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.terminal", "true"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.timezone", "Pacific/Samoa"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.triggerinterval", "15m"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.0.triggerlimit", "15"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.%", "7"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.emails.#", "1"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.emails.0", "test@logdna.com"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.immediate", "false"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.operator", "absence"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.terminal", "true"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.timezone", "Pacific/Samoa"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.triggerinterval", "15m"),
+					resource.TestCheckResourceAttr("logdna_view.new", "email_channel.1.triggerlimit", "15"),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.0", host3),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.1", host4),
+					resource.TestCheckResourceAttr("logdna_view.new", "hosts.0", host3),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.0", levels3),
+					resource.TestCheckResourceAttr("logdna_view.new", "levels.1", levels4),
+					resource.TestCheckResourceAttr("logdna_view.new", "pagerduty_channel.#", "0"),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.#", "2"),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.0", tags3),
+					resource.TestCheckResourceAttr("logdna_view.new", "tags.1", tags4),
 					resource.TestCheckResourceAttr("logdna_view.new", "webhook_channels.#", "0"),
 				),
 			},
