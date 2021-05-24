3417a60cfa1779af1ca6bb888a46157b0a88edd9
Darin Spivey <darin.spivey@logdna.com>
2021-05-24 10:32:44 -0400

    style: go fmt alighment changes

--- logdna/provider.go
+++ logdna/provider.go
@@ -9,7 +9,7 @@ import (
 
 type providerConfig struct {
 	serviceKey string
-	Host        string
+	Host       string
 	httpClient *http.Client
 }
 
@@ -41,7 +41,7 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 
 	return &providerConfig{
 		serviceKey: servicekey,
-		Host:        url,
+		Host:       url,
 		httpClient: &http.Client{Timeout: 15 * time.Second},
 	}, nil
 }
